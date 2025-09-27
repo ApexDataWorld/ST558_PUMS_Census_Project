0360550ffe38727787d9661daa254849f62568b8
jafericy-statistics <jafericy@ncsu.edu>
2025-09-27 16:17:57 -0400

    Create test_script.R
    Testing

---
+++ test_script.R
@@ -0,0 +1,9 @@
+library(httr)
+library(jsonlite)
+library(tibble)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+
+
+example_url <- "https://api.census.gov/data/2022/acs/acs1/pums?get=SEX,PWGTP,MAR&SCHL=24"
