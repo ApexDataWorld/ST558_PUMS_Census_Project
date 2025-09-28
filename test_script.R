16ff39031a983f251201d6dc70da97f995f22107
jafericy-statistics <jafericy@ncsu.edu>
2025-09-28 14:41:44 -0400

    Delete test_script.R
    deleting file here as well to remove merge error. File was created to test access to repo.

--- test_script.R
+++
@@ -1,9 +0,0 @@
-library(httr)
-library(jsonlite)
-library(tibble)
-library(dplyr)
-library(purrr)
-library(ggplot2)
-
-
-example_url <- "https://api.census.gov/data/2022/acs/acs1/pums?get=SEX,PWGTP,MAR&SCHL=24"
